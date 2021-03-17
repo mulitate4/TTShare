9944be538c18f570f4485beecb0f3e27a2b27c1d
Muwulitate4 <30702771+mulitate4@users.noreply.github.com>
2021-03-17 15:31:39 +0530

    v1 complete

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# TTShare_webapp
+# TTShare Website
+This repo contains the code for the frontend (website) for the TTShare API built by [Gee842](https://github.com/gee842)
+
+# Links
+- [Website]()
+- [Frontend Repo]() (this repository)
+- [Backend Repo]()
---
+++ main.js
@@ -0,0 +1,190 @@
+<<<<<<< HEAD
+let inputStr = document.querySelector(".inputStr");
+let downloadButton = document.querySelector(".downloadButton");
+let playButton = document.querySelector(".playButton");
+let copyButton = document.querySelector(".copyButton");
+let audio = document.querySelector(".TTS");
+let ID = "";
+
+copyButton.style.display = "none"
+
+
+async function getID(TTSstr){
+    ID = await fetch("http://13.229.216.148:8001/save?text=" + TTSstr)
+    .then((res) => res.text());
+    
+    return ID;
+}
+
+async function main(type="download"){
+    let TTSstr = inputStr.value;
+    if (TTSstr == "") return
+    
+    if (ID == ""){
+        ID = await getID(TTSstr);
+    }
+    
+    downloadURL = "http://13.229.216.148:8001/mp3s?tts_id="+ID;
+    
+    if (type=="download"){
+        downloadWindow = window.open(downloadURL, "_blank")
+        window.setInterval(()=>{
+            downloadWindow.close()
+        }, 1500);
+
+        copyButton.style.display = "block"
+    }
+    
+    else if (type=="play"){
+        audio.src = downloadURL;
+        audio.play();
+
+        copyButton.style.display = ""
+    }
+
+    else if (type=="copy"){
+        let dummyElem = document.createElement("textarea");
+        document.body.appendChild(dummyElem);
+        dummyElem.value = downloadURL;
+        dummyElem.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummyElem)
+
+        alert("Link copied to clipboard!")
+    }
+}
+
+downloadButton.addEventListener('click', () => {
+    main("download");
+});
+
+playButton.addEventListener('click', () => {
+    main("play");
+});
+
+copyButton.addEventListener('click', () => {
+    main("copy");
+});
+
+=======
+<<<<<<< HEAD
+let inputStr = document.querySelector(".inputStr");
+let downloadButton = document.querySelector(".downloadButton");
+let playButton = document.querySelector(".playButton");
+let copyButton = document.querySelector(".copyButton");
+let audio = document.querySelector(".TTS");
+let ID = "";
+
+async function getID(TTSstr){
+    ID = await fetch("http://13.229.216.148:8001/save?text=" + TTSstr)
+    .then((res) => res.text());
+
+    return ID;
+}
+
+async function main(type="download"){
+    let TTSstr = inputStr.value;
+        if (TTSstr == "") return
+
+    if (ID == ""){
+        ID = await getID(TTSstr);
+    }
+
+    downloadURL = "http://13.229.216.148:8001/mp3s?tts_id="+ID;
+
+    if (type=="download"){
+        downloadWindow = window.open(downloadURL, "_blank")
+        window.setInterval(()=>{
+            downloadWindow.close()
+        }, 1500);
+    }
+
+    else if (type=="play"){
+        audio.src = downloadURL;
+        audio.play();
+    }
+
+    else if (type=="copy"){
+        let dummyElem = document.createElement("textarea");
+        document.body.appendChild(dummyElem);
+        dummyElem.value = downloadURL;
+        dummyElem.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummyElem)
+
+        alert("Link copied to clipboard!")
+    }
+}
+
+downloadButton.addEventListener('click', () => {
+    main("download");
+});
+
+playButton.addEventListener('click', () => {
+    main("play");
+});
+
+copyButton.addEventListener('click', () => {
+    main("copy");
+=======
+let inputStr = document.querySelector(".inputStr");
+let downloadButton = document.querySelector(".downloadButton");
+let playButton = document.querySelector(".playButton");
+let copyButton = document.querySelector(".copyButton");
+let audio = document.querySelector(".TTS");
+let ID = "";
+
+async function getID(TTSstr){
+    ID = await fetch("http://13.229.216.148:8001/save?text=" + TTSstr)
+    .then((res) => res.text());
+
+    return ID;
+}
+
+async function main(type="download"){
+    let TTSstr = inputStr.value;
+        if (TTSstr == "") return
+
+    if (ID == ""){
+        ID = await getID(TTSstr);
+    }
+
+    downloadURL = "http://13.229.216.148:8001/mp3s?tts_id="+ID;
+
+    if (type=="download"){
+        downloadWindow = window.open(downloadURL, "_blank")
+        window.setInterval(()=>{
+            downloadWindow.close()
+        }, 1500);
+    }
+
+    else if (type=="play"){
+        audio.src = downloadURL;
+        audio.play();
+    }
+
+    else if (type=="copy"){
+        let dummyElem = document.createElement("textarea");
+        document.body.appendChild(dummyElem);
+        dummyElem.value = downloadURL;
+        dummyElem.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummyElem)
+
+        alert("Link copied to clipboard!")
+    }
+}
+
+downloadButton.addEventListener('click', () => {
+    main("download");
+});
+
+playButton.addEventListener('click', () => {
+    main("play");
+});
+
+copyButton.addEventListener('click', () => {
+    main("copy");
+>>>>>>> f370fa15d79bbb070bfdc42654fe3754b7dfe3f9
+});
+>>>>>>> 148660f1a568a592f9315cd299b360d89fce64a3
